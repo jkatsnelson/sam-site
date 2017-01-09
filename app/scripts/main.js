f5c207dbaf9ed45a6ca32966b6ad47c460189dcd
John Katsnelson <johnkatsnelson@gmail.com>
2017-01-08 22:43:00 -0800

    touch, swipe, larger touch spot

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,51 +1,50 @@
 // from https://github.com/bootstrapthemesco/bootstrap-carousel-touch-slider
 
-( function ( $ ) {
-    'use strict';
 
-    $.fn.bsTouchSlider = function ( options ) {
-        var carousel = $( '.carousel' );
-        return this.each( function ( ) {
-
-            function doAnimations( elems ) {
-                //Cache the animationend event in a variable
-                var animEndEv = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-                elems.each( function ( ) {
-                    var $this = $( this ),
-                        $animationType = $this.data( 'animation' );
-                    $this.addClass( $animationType ).one( animEndEv, function ( ) {
-                        $this.removeClass( $animationType );
-                    } );
-                } );
-            }
-
-            //Variables on page load
-            var $firstAnimatingElems = carousel.find( '.item:first' ).find( '[data-animation ^= \'animated\']' );
-            //Animate captions in first slide on page load
-            doAnimations( $firstAnimatingElems );
-            //Other slides to be animated on carousel slide event
-            carousel.on( 'slide.bs.carousel', function ( e ) {
-                var $animatingElems = $( e.relatedTarget ).find( '[data-animation ^= \'animated\']' );
-                doAnimations( $animatingElems );
-            } );
-            //swipe initial
-            $( '.carousel .carousel-inner' ).swipe( {
-                swipeLeft: function ( event, direction, distance, duration, fingerCount ) {
-                    this.parent( ).carousel( 'next' );
-                },
-                swipeRight: function ( ) {
-                    this.parent( ).carousel( 'prev' );
-                },
-                threshold: 0
-            } );
-
-        } );
-    };
-
-
-} )( jQuery );
 $(document).ready(function() {
-  $('button.btn').click(function () {
-    $('.carousel').bsTouchSlider();
-  });
+  fixAnimations();
+  addSwipe();
 })
+
+function fixAnimations() {
+  var carousel = $('.carousel');
+  carousel.each(function() {
+
+      function doAnimations( elems ) {
+          //Cache the animationend event in a variable
+          var animEndEv = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+          elems.each( function ( ) {
+              var $this = $( this ),
+                  $animationType = $this.data( 'animation' );
+              $this.addClass( $animationType ).one( animEndEv, function ( ) {
+                  $this.removeClass( $animationType );
+              } );
+          } );
+      }
+
+      //Variables on page load
+      var $firstAnimatingElems = carousel.find( '.item:first' ).find( '[data-animation ^= \'animated\']' );
+      //Animate captions in first slide on page load
+      doAnimations( $firstAnimatingElems );
+      //Other slides to be animated on carousel slide event
+      carousel.on( 'slide.bs.carousel', function ( e ) {
+          var $animatingElems = $( e.relatedTarget ).find( '[data-animation ^= \'animated\']' );
+          doAnimations( $animatingElems );
+      } );
+
+  });
+}
+
+// this isn't being used, but im leaving it for later
+
+function addSwipe() {
+  $( '.carousel .carousel-inner' ).swipe( {
+      swipeLeft: function () {
+          this.parent( ).carousel( 'next' );
+      },
+      swipeRight: function ( ) {
+          this.parent( ).carousel( 'prev' );
+      },
+      threshold: 0
+  } );
+}
