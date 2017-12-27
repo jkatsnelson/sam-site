72308d13099255a1c624fbdcd149bd38cd0dc085
John Katsnelson <johnkatsnelson@gmail.com>
2017-12-27 12:21:28 -0800

    removes slideout.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
     trigger: 'hover'
   });
   $('.carousel').carousel('pause');
-  navSlideout();
   fixAnimations();
   addSwipe();
   if (window.matchMedia('(min-width: 992px)').matches) {
@@ -48,47 +47,6 @@ function fixCarouselArrows() {
     }
   }
 };
-
-function navSlideout() {
-  var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu')
-  });
-
-  $('#menu a').click(function () {
-    slideout.toggle();
-  });
-
-  // when the page loads, disable touch because it conflicts with carousels
-  // when the menu is open, enable touch so its easy to get out
-  // when the menu closes again, re-disable touch
-  slideout.disableTouch();
-  slideout.on('open', function() {
-    slideout.enableTouch();
-  });
-  slideout.on('close', function() {
-    slideout.disableTouch();
-  });
-
-  document.querySelector('.burger').addEventListener('click', function() {
-    slideout.toggle();
-  });
-
-  // document.querySelector('.menu').addEventListener('click', function(eve) {
-  //   if (eve.target.nodeName === 'A') { slideout.close(); }
-  // });
-
-  slideout.on('beforeopen', function() {
-    document.querySelector('.nav-bar').className += ' open';
-    document.querySelector('#panel .overlay').className += ' active';
-  });
-
-
-  slideout.on('beforeclose', function() {
-    document.querySelector('.nav-bar').className = 'nav-bar';
-    document.querySelector('#panel .overlay').className = 'overlay';
-  });
-}
 
 function fixAnimations() {
   var carousel = $('.carousel');
